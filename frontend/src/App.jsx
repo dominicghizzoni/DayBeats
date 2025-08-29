83cee581b893264ac4b75fd8f0cd6f3927bd1e7d
Dominic Ghizzoni <dominicghizzoni@ufl.edu>
2025-08-28 23:53:04 -0400

    Added navbar to page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Navbar } from 'react-bootstrap';
 import CalendarPage from './CalendarPage';
 import SongSelect from './SongSelect';
 import Login from './Login';
@@ -11,20 +11,24 @@ function App() {
 
   return (
     <div className="App">
-      <Container className="d-flex justify-content-end mt-3" style={{maxWidth: '1920px'}}>
-        <Button variant="outline-primary" onClick={() => navigate("/calendar")}>
-          View Calendar
-        </Button>
-        <Button variant="outline-primary" onClick={() => navigate("/")}>
-         Song Select
-        </Button>
-      </Container>
+      <Navbar className="top-bar" fixed="top">
+        <Container className="justify-content-end">
+          <Button variant="outline-primary" onClick={() => navigate("/calendar")}>
+            View Calendar
+          </Button>
+          <Button variant="outline-primary" onClick={() => navigate("/")}>
+            Song Select
+          </Button>
+        </Container>
+      </Navbar>
 
+    <div style={{ paddingTop: "70px" }}>
       <Routes>
         <Route path="/" element={<SongSelect />} />
         <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      </div>
     </div>
   );
 }
